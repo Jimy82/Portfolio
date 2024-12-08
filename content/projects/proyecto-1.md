80b81b18dd34ea254d9fbe20038011b4a8d190e8
Jaime Frutos Perez-Surio <jaimefrutosperez-surio@MacBook-Air-de-Jaime.local>
2024-12-08 19:02:49 +0100

    Commit inicial

---
+++ content/projects/proyecto-1.md
@@ -0,0 +1,20 @@
+---
+title: "Proyecto 1"
+description: "Página enfocada en el desarrollo sostenible."
+date: 2024-12-01
+tags: ["tecnología", "sostenibilidad"]
+---
+
+## SITIO DE DESARROLLO SOSTENIBLE
+
+**Descripción**:  
+En este proyecto creé un sitio web orientado a la promoción del desarrollo sostenible. La página ofrece información, recursos y herramientas para concienciar sobre la importancia de la sostenibilidad, destacando prácticas cotidianas y estrategias a nivel corporativo e institucional.  
+La experiencia de usuario se ha trabajado para que la navegación sea intuitiva y el contenido sea accesible, fomentando un cambio positivo a nivel social y medioambiental.
+
+**Tecnologías utilizadas**:  
+- HTML  
+- CSS (con énfasis en diseño responsivo)  
+- JavaScript (interacciones dinámicas, carga de datos de APIs relacionadas con sostenibilidad)
+
+**Enlace**: [Visitar el proyecto 1](https://jimy82.github.io/Sostenibilidad-en-Accion-001/)
+
---
+++ public/searchindex.en.js
@@ -0,0 +1,104 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "¡Hola! Soy Jaime Soy desarrollador de aplicaciones web, con experiencia en Hugo. Aquí puedes encontrar mis proyectos y logros destacados.\n🌟 ¡Explora mi portafolio y no dudes en contactarme!",
+    "description": "Explora mis proyectos y conoce más sobre mí.",
+    "tags": [],
+    "title": "Bienvenido a Mi Portafolio",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio",
+    "content": "Jaime Frutos Pérez-Surio Dirección: Pº Ruiseñores 22-24, 50006 Zaragoza\nTeléfono: 670 377 634\nEmail: jaime.frutos.perez.surio@gmail.com\nPerfil Profesional con experiencia en atención al cliente, gestión administrativa y soporte técnico, actualmente finalizando el Grado Superior en Desarrollo de Aplicaciones Web. Competente en tecnologías de desarrollo web, bases de datos y herramientas digitales, con un enfoque en la creación de soluciones innovadoras y eficientes. Destacado por mi adaptabilidad, empatía y habilidades para resolver problemas, tanto técnicos como interpersonales.\nExperiencia Agente de Call Center, Ibercaja (2022–Actualidad)\nResolución de incidencias y consultas de clientes mediante atención telefónica. Manejo de herramientas digitales específicas para la gestión bancaria. Coordinación con equipos internos para ofrecer soluciones rápidas y efectivas. Responsable Técnico, Brico Depot (Mayo 2009–2022)\nGestión del stock, reposición y atención personalizada al cliente. Supervisión de la logística interna de productos en tienda. Administrativo (Prácticas), Seguros Mapfre (Junio 2007–Marzo 2008)\nGestión y archivo de documentación. Atención personalizada y telefónica a clientes para contratación de seguros. Operario Logístico, Transportes Azkar (Septiembre 2005–Mayo 2007)\nRecepción, control y distribución de mercancías. Preparación de pedidos mediante picking por radiofrecuencia. Formación Grado Superior en Desarrollo de Aplicaciones Web, Instituto Miguel Catalán (Zaragoza, España) — 2022–Actualidad Técnico Superior en Administración y Finanzas, Instituto Miguel Catalán (Zaragoza, España) — 2008 Aptitudes Desarrollo web: HTML, CSS, JavaScript Bases de datos: SQL y MySQL Gestión de proyectos con Git y GitHub Microsoft Office: Word, Excel, PowerPoint Habilidades interpersonales: atención al cliente, resolución de problemas, adaptabilidad y empatía Idiomas Inglés: Nivel Medio Proyectos Personales Portfolio Web Personal: Creación de un sitio web con Hugo utilizando el tema Relearn, incluyendo información sobre mis habilidades, formación y proyectos. Intereses Tecnología y desarrollo web Surfcasting y actividades al aire libre Aprendizaje continuo en programación y nuevas herramientas digitales",
+    "description": "Experiencia, formación y habilidades de Jaime Frutos Pérez-Surio",
+    "tags": [],
+    "title": "Curriculum",
+    "uri": "/curriculum/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Projects",
+    "uri": "/projects/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio \u003e Projects",
+    "content": "SITIO DE DESARROLLO SOSTENIBLE Descripción:\nEn este proyecto creé un sitio web orientado a la promoción del desarrollo sostenible. La página ofrece información, recursos y herramientas para concienciar sobre la importancia de la sostenibilidad, destacando prácticas cotidianas y estrategias a nivel corporativo e institucional.\nLa experiencia de usuario se ha trabajado para que la navegación sea intuitiva y el contenido sea accesible, fomentando un cambio positivo a nivel social y medioambiental.\nTecnologías utilizadas:\nHTML CSS (con énfasis en diseño responsivo) JavaScript (interacciones dinámicas, carga de datos de APIs relacionadas con sostenibilidad) Enlace: Visitar el proyecto 1",
+    "description": "Página enfocada en el desarrollo sostenible.",
+    "tags": [
+      "Tecnología",
+      "Sostenibilidad"
+    ],
+    "title": "Proyecto 1",
+    "uri": "/projects/proyecto-1/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Sostenibilidad",
+    "uri": "/tags/sostenibilidad/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Tecnología",
+    "uri": "/tags/tecnolog%C3%ADa/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Diseño",
+    "uri": "/tags/dise%C3%B1o/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio \u003e Projects",
+    "content": "EBRO TRIGO SLU Descripción: En este proyecto desarrolle una pagina web para una prestigiosa empresa de harinas. Apliqué un enfoque artesanal tanto en el concepto como en el despliegue, un proyecto hecho con ilusion y empatia.\nTecnologías utilizadas:\nHTML CSS JavaScript Enlace: Visitar el proyecto 2",
+    "description": "Descripción del segundo proyecto.",
+    "tags": [
+      "Tecnología",
+      "Diseño"
+    ],
+    "title": "Proyecto 2",
+    "uri": "/projects/proyecto-2/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio",
+    "content": "¡Hablemos! Estoy disponible para proyectos freelance, consultas o cualquier tipo de colaboración interesante.\n¡No dudes en ponerte en contacto!\n📧 Email: jaime.frutos.perez.surio@gmail.com\n🔗 LinkedIn: JaimeFrutos\n🐙 GitHub: Jimy82\nSi lo prefieres, también puedes escribirme directamente desde el formulario de contacto (si decides agregarlo más adelante).",
+    "description": "¡Hablemos! Estoy disponible para proyectos freelance, consultas o cualquier tipo de colaboración interesante.\n¡No dudes en ponerte en contacto!\n📧 Email: jaime.frutos.perez.surio@gmail.com\n🔗 LinkedIn: JaimeFrutos\n🐙 GitHub: Jimy82\nSi lo prefieres, también puedes escribirme directamente desde el formulario de contacto (si decides agregarlo más adelante).",
+    "tags": [],
+    "title": "Contacto",
+    "uri": "/contact/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenido a Mi Portafolio",
+    "content": "¡Hola, soy Jaime! Soy un desarrollador de aplicaciones web con experiencia en Hugo. Me apasiona trabajar en proyectos nuevos y siempre busco formas de mejorar mis habilidades. Disfruto de todo el proceso de desarrollo, desde la conceptualización hasta el despliegue del producto final.\nHabilidades:\nHugo PHP JavaScript MySQL HTML CSS Formación Desarrollo de Aplicaciones Web en IES Los Enlaces (Zaragoza) Administración y Finanzas en IES Miguel Catalán (Zaragoza) [Cursos o certificaciones relevantes] Experiencia He colaborado con diversos clientes en el desarrollo de páginas web, ajustándome a las necesidades específicas de cada proyecto. Me gusta mantenerme al día con las nuevas tecnologías y herramientas para mejorar continuamente la experiencia y la calidad de mis proyectos.",
+    "description": "¡Hola, soy Jaime! Soy un desarrollador de aplicaciones web con experiencia en Hugo. Me apasiona trabajar en proyectos nuevos y siempre busco formas de mejorar mis habilidades. Disfruto de todo el proceso de desarrollo, desde la conceptualización hasta el despliegue del producto final.\nHabilidades:\nHugo PHP JavaScript MySQL HTML CSS Formación Desarrollo de Aplicaciones Web en IES Los Enlaces (Zaragoza) Administración y Finanzas en IES Miguel Catalán (Zaragoza) [Cursos o certificaciones relevantes] Experiencia He colaborado con diversos clientes en el desarrollo de páginas web, ajustándome a las necesidades específicas de cada proyecto. Me gusta mantenerme al día con las nuevas tecnologías y herramientas para mejorar continuamente la experiencia y la calidad de mis proyectos.",
+    "tags": [],
+    "title": "Sobre Mí",
+    "uri": "/about/index.html"
+  }
+]
